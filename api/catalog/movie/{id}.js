fbbcff587251b7536825f89c3ea613c537dcd1a0
ch0s3n1nav <chachagangster@googlemail.com>
2025-09-05 21:34:33 +0100

    Create {id}.js

---
+++ api/catalog/movie/{id}.js
@@ -0,0 +1,17 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'application/json');
+  
+  const { id } = req.query;
+  
+  const streams = [{
+    title: "Real-Debrid Stream",
+    name: "Direct Stream", 
+    url: `https://real-debrid.com/stream/${id}`,
+    behaviorHints: {
+      notWebReady: true
+    }
+  }];
+  
+  res.json({ streams });
+}
