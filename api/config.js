7001bff3934ee189e8819c72db43c1fe33b2d2a5
ch0s3n1nav <chachagangster@googlemail.com>
2025-09-06 02:05:52 +0100

    Create config.js

---
+++ api/config.js
@@ -0,0 +1,15 @@
+// Simple in-memory config storage (in production, use a database)
+let userConfig = {
+  apiKey: null,
+  configured: false
+};
+
+export function getConfig() {
+  return userConfig;
+}
+
+export function setConfig(newConfig) {
+  userConfig = { ...userConfig, ...newConfig };
+  userConfig.configured = !!userConfig.apiKey;
+  return userConfig;
+}
