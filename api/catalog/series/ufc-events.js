5f343c656f32231d88853687b874a79e750fa26d
ch0s3n1nav <chachagangster@googlemail.com>
2025-09-06 22:17:58 +0100

    Update ufc-events.js

--- api/catalog/series/ufc-events.js
+++ api/catalog/series/ufc-events.js
@@ -2,11 +2,9 @@ const { REAL_DEBRID_API_KEY } = process.env;
 
 module.exports = async (req, res) => {
   try {
-    if (!REAL_DEBRID_API_KEY) {
-      throw new Error('Real-Debrid API key is not configured');
-    }
-
-    // For UFC series, return empty for now
+    // For UFC series, we'll return an empty array
+    // You might need to implement your own logic to detect UFC series content
+    
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.json({ metas: [] });
