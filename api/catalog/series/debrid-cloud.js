282d29da07f5b085674250b4376a270d85d3010a
ch0s3n1nav <chachagangster@googlemail.com>
2025-09-07 01:12:10 +0100

    Update and rename debrid-cloud.json.js to debrid-cloud.js

--- api/catalog/series/debrid-cloud.js
+++ api/catalog/series/debrid-cloud.js
@@ -2,6 +2,8 @@ const { REAL_DEBRID_API_KEY } = process.env;
 
 module.exports = async (req, res) => {
   try {
+    console.log('Series Cloud endpoint called');
+    
     // Set CORS headers
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -14,8 +16,9 @@ module.exports = async (req, res) => {
       return;
     }
 
-    // For series, we'll return an empty array since Real-Debrid doesn't 
-    // differentiate between movie and series in their API
+    // For series, return empty array since Real-Debrid doesn't differentiate
+    // between movie and series content in their API
+    console.log('Returning empty series catalog (not implemented yet)');
     res.json({ metas: [] });
   } catch (error) {
     console.error('Error in series catalog:', error);
